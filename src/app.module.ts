ca6b5984ace87d17cca690736054e79c98326ce2
Jesse-Bett <jessekip21@gmail.com>
2022-07-05 12:38:38 +0300

    Deployed to Heroku

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { booksModule } from './books/books.module';
@@ -13,7 +14,7 @@ import { stockModule } from './stock/stock.module';
 
 @Module({
   imports: [ServeStaticModule.forRoot({
-    rootPath: join(__dirname, '.', 'landingPage'),
+    rootPath: join(__dirname, '..', 'client'),
   }),booksModule,authorModule,stockModule, MongooseModule.forRoot('mongodb+srv://test:123456789password@cluster0.eawpi.mongodb.net/bookstoreIMS?retryWrites=true&w=majority')], // Database ConnectionString
   controllers: [AppController],
   providers: [AppService], 
