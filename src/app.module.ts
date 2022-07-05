07ba9f23bc40c2865e5cdc0c9f2f55128ae384ad
Jesse-Bett <jessekip21@gmail.com>
2022-07-05 12:38:38 +0300

    Deployed to Heroku

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { booksModule } from './books/books.module';
