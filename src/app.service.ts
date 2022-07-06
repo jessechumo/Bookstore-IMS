ebcb2d24019d19bc7a00c4b16d6dfdcf8938a2d5
Jesse-Bett <jessekip21@gmail.com>
2022-07-06 09:04:27 +0300

    And we're live

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Welcome to the bookstore inventory management system!';
+    return 'Welcome to the bookstore inventory management system! Please navigate to /api to view the documentation';
   }
 }
